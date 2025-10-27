1df548894aa5778339f274a78b103f9af9504909
Muhammad <mf9555759@gmail.com>
2025-10-27 20:00:11 +0800

    fetch image, add button

--- ecommerce-app/app/page.tsx
+++ ecommerce-app/app/page.tsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 export default async function Home() {
   const products = await stripe.products.list({
@@ -11,6 +14,29 @@ export default async function Home() {
 
   console.log(products)
   return(
-      <div>y</div>
+      <div>
+        <section>
+          <div>
+
+            <div>
+              <h2>Welcome to My Ecommerce</h2>
+              <p>Discover the lastest product at the best prices</p>
+              <Button asChild variant="default">
+                <Link href="/products.tsx">Browse All Products</Link>
+              </Button>
+            </div>
+            <Image 
+              alt="Banner img/"
+              width={450} 
+              height={450} 
+              src={products.data[0].images[0]}
+            />
+            
+          </div>
+        </section>
+        <section>
+
+        </section>
+      </div>
   );
 }
